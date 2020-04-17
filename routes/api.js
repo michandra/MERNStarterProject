392ed2e53d765cdaa57d55e952c683977d733635
chandramohans <chandramohan.subbiah@gmail.com>
2020-04-17 16:17:00 +0530

    added Survey POST route

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var authRouter = require("./auth");
 var bookRouter = require("./book");
+var surveyRouter = require("./survey");
 
 var app = express();
 
 app.use("/auth/", authRouter);
 app.use("/book/", bookRouter);
+app.use("/survey/", surveyRouter);
 
 module.exports = app;
